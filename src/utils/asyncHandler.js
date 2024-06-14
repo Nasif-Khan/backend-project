daf86277af2b68dee10aeac1379fa4dada8dd688
Nasif-Khan <nasif.sajid.khan@gmail.com>
2024-06-15 03:04:10 +0530

    update async handler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,24 @@
-const asyncHandler =  (requesHandler) => {
-    (req, res, next) =>{
-        Promise.resolve(requesHandler(req, res, next)).catch((err) => next(err));
+// The purpose of this utility is to simplify error handling in asynchronous 
+// Express route handlers or middleware. 
+
+const asyncHandler =  (requestHandler) => {
+    return (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
     }
 }
 
 export {asyncHandler}
+
+
+
+// const asyncHandler = (fn) => (req, res, next) => {
+//     try{
+//         await fn (req, res, next);
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             message: error.message || 'An error occurred',
+//             success: false
+//         })
+//     }
+// }

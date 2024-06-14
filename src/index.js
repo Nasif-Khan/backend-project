841503670d58bce6bc5652718f7deb9e99c9f6af
Nasif-Khan <nasif.sajid.khan@gmail.com>
2024-06-15 02:36:55 +0530

    add express and utils functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({path: './env'});
+dotenv.config({path: "./env"});
 
 connectDB()
+.then(() => {
+    app.on('error', (error) => {
+        console.log(`Server error: ${error}`);
+    });
+    app.listten(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((error) => {
+    console.log(`Connection to DB failed: ${error}`);
+})
